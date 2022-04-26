ab535a9bf82438389e21f5fb73868487906f00e1
XiaoWenPang <pangxiaowen1314@gmail.com>
2022-04-26 11:41:08 +0800

    5-4 Kotlin抽象类与接口

--- app/src/main/java/com/ivan/kotlindemo/KotlinClass.kt
+++ app/src/main/java/com/ivan/kotlindemo/KotlinClass.kt
@@ -2,6 +2,7 @@ package com.ivan.kotlindemo
 
 import android.view.View
 import java.util.*
+import kotlin.collections.ArrayList
 
 fun main() {
     Dog(11)
@@ -11,6 +12,8 @@ fun main() {
     val test = Test()
     test.setUp()
     test.test()
+    testDataUtil()
+    testStudent()
 
 }
 
@@ -94,4 +97,126 @@ class Test {
         //::表示创建成员引用或类引用
         if(::shop.isInitialized) println(shop.address)
     }
+}
+
+/**
+ * 抽象类的定义和使用
+ */
+abstract class Printer() {
+    abstract fun print()
+}
+class FilePrinter: Printer() {
+    override fun print() {
+        println("print file")
+    }
+}
+
+/**
+ * 接口的定义和使用
+ * Kotlin中接口可以有自己的方法实现
+ */
+interface Study {
+    val time: Int //抽象的，未实现的属性要在实现接口类的主构造方法中override
+    fun discuss()
+    fun learnCourse() {
+        println("Android 架构师")
+    }
+}
+class StudyAS(override val time: Int) : Study {
+    override fun discuss() {
+        TODO("Not yet implemented")
+    }
+}
+
+/**
+ * 两个接口有相同方法的情况
+ */
+interface A {
+    fun foo() {
+        println("A")
+    }
+}
+interface B {
+    fun foo() {
+        println("B")
+    }
+}
+class D: A, B {
+    override fun foo() {
+        //super.foo() //报错，因为不知道调用A的foo()还是B的foo()
+        super<A>.foo() //指定调用谁的方法
+    }
+}
+
+/**
+ * 数据类
+ * 1.主构造方法必须有参数
+ * 2.数据类型不能定义为open,因为数据类不能被继承
+ */
+data class Address(var name: String, val number: Int) {
+    var city: String = ""
+    fun print() {
+        println(city)
+    }
+}
+
+/**
+ * 对象表达式与对象声明
+ */
+open class Address2(name: String) {
+    open fun print() {
+
+    }
+}
+class Shop2{
+    var address: Address2 ?= null
+    fun addAddress(address: Address2) {
+        this.address = address
+    }
+}
+fun test3() {
+    //在不创建新的子类的情况下，对类进行一些修改
+    //如果超类型有一个构造方法，则必须传递适当的构造方法参数给它
+    Shop2().addAddress(object : Address2("Android") {
+        override fun print() {
+            super.print()
+        }
+    })
+}
+fun foo() {
+    //如果需要创建一个新的类，不一定需要创建对象，也可以采用这种对象声明的方式创建一个类
+    //这是一个匿名对象
+    val adHoc = object {
+        var x: Int = 0
+        var y: Int = 0
+    }
+    println(adHoc.x + adHoc.y)
+}
+
+object DataUtil {
+    //可以将isEmpty理解为DataUtil中的一个静态对象
+    fun<T> isEmpty(list: ArrayList<T>?): Boolean {
+        return list?.isEmpty() ?: false //不确定list是否为空，所以加个？
+    }
+}
+fun testDataUtil() {
+    val list = arrayListOf("1")
+    println(DataUtil.isEmpty(list))
+}
+
+/**
+ * 伴生对象
+ * 和Java的静态字段和静态方法相似
+ */
+class Student(val name: String) {
+    companion object{
+        val student = Student("Android")
+        fun study() {
+            println("Android 架构师")
+        }
+    }
+}
+fun testStudent() {
+    println(Student.student)
+    Student.study()
 }

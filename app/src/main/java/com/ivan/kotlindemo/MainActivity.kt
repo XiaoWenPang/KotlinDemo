31aeb6cf21f47ce41e43baf0f179a00bb0594714
XiaoWenPang <pangxiaowen1314@gmail.com>
2022-04-27 11:53:57 +0800

    6-5 Kotlin扩展案例

--- app/src/main/java/com/ivan/kotlindemo/MainActivity.kt
+++ app/src/main/java/com/ivan/kotlindemo/MainActivity.kt
@@ -3,15 +3,17 @@ package com.ivan.kotlindemo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val textView = find<TextView>(R.id.text)
+        //val textView = find<TextView>(R.id.text)
+
         R.id.text.onClick(this) {
-            textView.text = "Kotlin Extension"
+            text.text = "Kotlin Extension"
         }
     }
 }
